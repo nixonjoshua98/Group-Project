d56d1d561d2c61bd7cebd8a469ebd40c7686847c
wmcyamu <48129057+wmcyamu@users.noreply.github.com>
2019-03-01 18:32:37 +0000

    AxisBasedMovement
    the box can move

---
+++ StudentLifeGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpforce = 10f;
+    public float jumpCD = 2f;
+
+    private float nextJumpTime = 0;
+
+    private Rigidbody2D myRB;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        // Get the horizontal and vertical axis
+        // defult map to arrow keys 
+        // The value is in the range -1 to 1
+        float vert = Input.GetAxis("Vertical") * jumpforce;
+        float hori = Input.GetAxis("Horizontal") * speed;
+
+        //per second movement insetad of frame
+        vert *= Time.deltaTime;
+        hori *= Time.deltaTime;
+        transform.Translate(hori, vert, 0);
+
+    }
+}
